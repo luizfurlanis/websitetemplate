fce47b6a087dc383d80529be857f0b13218dc2f4
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2024-12-13 14:58:11 -0300

    produtos via api

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,19 @@
-// Obtém a navbar
-const navbar = document.querySelector('.navbar');
-const navlink = document.querySelectorAll('.nav-links');
-
-// Função para verificar o scroll
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 1) {  
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
-
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        navbar.classList.add('active');
+fetch('https://fakestoreapi.com/products?limit=5')
+    .then(res => res.json())
+    .then((json) => {
+        console.log("Produtos recebidos da API:", json);
+        const ul = document.getElementById('listaProdutos'); // Seleciona a lista
+        // Adiciona cada produto como um item na lista
+        json.forEach((item) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
+                <img src="${item.image}" alt="${item.title}">
+                <h3>${item.title}</h3>
+                <p>$${item.price}</p>
+            `;
+            ul.appendChild(li); // Adiciona o item na lista
+        });
     })
-});
-
+    .catch((error) => {
+        console.error("Erro ao buscar produtos:", error);
+    });
