e1ed27084917e3c6d6109ef1a1baac99b73f0316
LuizFurlanis <122633468+luizfurlanis@users.noreply.github.com>
2025-02-05 17:15:27 -0300

    [feat] color selector working and correct names to the principal image.

---
+++ scripts/products.js
@@ -0,0 +1,59 @@
+
+// Constante para usar na troca das imagens pelo usuário
+const currentId = { 'imagem-central': 1, 'p2': 0 };
+const produtosIrisBotao = ["../assets/Images/Iris Botão/60x60/2 Quadro L 60x60     Ligth Grey    Iris Botão  .jpg",
+    "../assets/Images/Iris Botão/60x60/1 Quadro L 60x60 White    Iris Botão .jpg", "../assets/Images/Iris Botão/60x60/4 Quadro L 60x60    Purple   Iris Botão.jpg",
+    "../assets/Images/Iris Botão/60x60/3 Quadro L 60x60    Dark Grey    Iris Botão .jpg"];
+
+// Cosntantes de seleções usadas nas funções que selecionam os tamanhos e cores
+const colorButton = document.querySelectorAll('.product_color_button');
+const sizeButton = document.querySelectorAll('.product_selector_button');
+let choseColor = null;
+let choseSize = null;
+
+// Função para trocar a imagem nas setas
+function trocarImagem(direction, id, imagem) {
+    currentId[id] += direction;
+
+    if (currentId[id] < 0) {
+        currentId[id] = imagem.length - 1;
+    } else if (currentId[id] >= imagem.length) {
+        currentId[id] = 0;
+    }
+
+    document.getElementById(id).src = imagem[currentId[id]];
+}
+
+// Função para trocar as imagens com os botões das cores
+function trocarImagemCor(number, id, imagem) {
+    document.getElementById(id).src = imagem[number]
+}
+
+// Função para remover os selecionados usada nas funções colorButton e sizeButton
+function removechoses(buttons) {
+    buttons.forEach(button => button.classList.remove('chose'));
+}
+
+// Função para a seleção dos botões das cores
+colorButton.forEach(button => {
+    button.addEventListener('click', () => {
+        removechoses(colorButton);
+
+        button.classList.add('chose')
+
+        choseColor = button.querySelector('img').alt;
+        console.log('Cor selecionada:', choseColor)
+    });
+});
+
+// Função para a seleção dos botões dos tamanhos
+sizeButton.forEach(button => {
+    button.addEventListener('click', () => {
+        removechoses(sizeButton);
+
+        button.classList.add('chose');
+
+        choseSize = button.querySelector('.size-span').textContent;
+        console.log('Tamanho escolhido:', choseSize)
+    });
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,29 @@
 // Variável para armazenar os produtos recebidos da API
 let productsData = [];
 
+// Fetch dos produtos da API
+fetch('http://127.0.0.1:5000/quadros')
+    .then(res => res.json())
+    .then((json) => {
+        console.log("Produtos recebidos da API:", json);
+        productsData = json; // Armazena os produtos recebidos
+
+        // Exibe todos os produtos inicialmente
+        displayProducts(productsData);
+
+        // Adiciona o evento de pesquisa para filtrar os produtos
+        const searchInput = document.querySelector('.busca');
+        searchInput.addEventListener('input', (event) => {
+            const searchQuery = event.target.value;
+            const filteredProducts = filterProducts(productsData, searchQuery);
+            displayProducts(filteredProducts); // Exibe os produtos filtrados
+        });
+    })
+    .catch((error) => {
+        console.error("Erro ao buscar produtos:", error);
+    });
+
+
 // Função para filtrar os produtos com base no valor da pesquisa
 function filterProducts(products, searchQuery) {
     return products.filter(item => 
@@ -26,81 +49,3 @@ function displayProducts(products) {
         ul.appendChild(li); // Adiciona o item na lista
     });
 }
-
-// Fetch dos produtos da API
-fetch('http://127.0.0.1:5000/quadros')
-    .then(res => res.json())
-    .then((json) => {
-        console.log("Produtos recebidos da API:", json);
-        productsData = json; // Armazena os produtos recebidos
-
-        // Exibe todos os produtos inicialmente
-        displayProducts(productsData);
-
-        // Adiciona o evento de pesquisa para filtrar os produtos
-        const searchInput = document.querySelector('.busca');
-        searchInput.addEventListener('input', (event) => {
-            const searchQuery = event.target.value;
-            const filteredProducts = filterProducts(productsData, searchQuery);
-            displayProducts(filteredProducts); // Exibe os produtos filtrados
-        });
-    })
-    .catch((error) => {
-        console.error("Erro ao buscar produtos:", error);
-    });
-
-
-
-    //Codigo que troca a visualização do produto
-const currentId = {'p1': 0, 'p2' : 0 };
-const produtosIrisBotao = ["../assets/Images/Iris Botão/60x60/2 Quadro L 60x60     Ligth Grey    Iris Botão  .jpg",
-    "../assets/Images/Iris Botão/60x60/1 Quadro L 60x60 White    Iris Botão .jpg", "../assets/Images/Iris Botão/60x60/4 Quadro L 60x60    Purple   Iris Botão.jpg", 
-    "../assets/Images/Iris Botão/60x60/3 Quadro L 60x60    Dark Grey    Iris Botão .jpg"];
-
-function trocarImagem(direction, id, imagem){
-    currentId[id] +=  direction;
-
-    if(currentId[id] < 0) {
-        currentId[id] = imagem.length - 1;
-    } else if (currentId[id] >= imagem.length) {
-        currentId[id] = 0;
-    }
-
-    document.getElementById(id).src = imagem[currentId[id]];
-}
-
-function trocarImagemCor(number, id, imagem){
-    document.getElementById(id).src = imagem[number]
-}
-// Codigo que seleciona os botoes de tamanho e cor do produto
-const colorButton = document.querySelectorAll('.product_color_button');
-const sizeButton = document.querySelectorAll('.product_selector_button');
-
-let choseColor = null;
-let choseSize = null;
-
-function removechoses(buttons) {
-    buttons.forEach(button => button.classList.remove('chose'));
-}
-
-colorButton.forEach(button => {
-    button.addEventListener('click', () => {
-        removechoses(colorButton);
-
-        button.classList.add('chose')
-
-        choseColor = button.querySelector('img').alt;
-        console.log('Cor selecionada:', choseColor)
-    });
-});
-
-sizeButton.forEach(button => {
-    button.addEventListener('click', () => {
-        removechoses(sizeButton);
-
-        button.classList.add('chose');
-
-        choseSize = button.querySelector('.size-span').textContent;
-        console.log('Tamanho escolhido:', choseSize)
-    });
-});
