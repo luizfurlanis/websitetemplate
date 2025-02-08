dc15a05cd18d3f1b8bf4c50cf74011c1985e831f
LuizFurlanis <furlanis.lu@gmail.com>
2025-02-08 12:31:51 -0300

    [feat] slider banners

---
+++ scripts/slider.js
@@ -0,0 +1,18 @@
+var radio = document.querySelector('.manual-btn')
+var count = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+
+function proximaImg() {
+    count++
+    if (count > 4) {
+        count = 1
+    }
+
+    document.getElementById('radio' + count).checked = true
+}
